c27da2bb0deaaa666685d8151b77aae35466eaf6
VassiliyRotar <vassiliyrotar@gmail.com>
2023-10-26 22:31:07 +0600

    first

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';  /* Если что удалить */
+
+
+const Footer = () => { 
+    return (
+      <div>
+          IT-Specialist
+      </div>
+    );
+  }
+
+  export default Footer;
