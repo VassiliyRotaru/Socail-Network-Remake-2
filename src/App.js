afaa5462c41a0cd99464666497bb55f49a0d93b2
VassiliyRotar <vassiliyrotar@gmail.com>
2023-10-31 00:15:25 +0600

    second

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React from 'react';  /* Если что удалить */
 import logo from './logo.svg';
 import './App.css';
-import Technologies from './Technologies';
-import Header from './Header';
-import Footer from './Footer';
-
 
 
 const App = () => { 
-  return (
+  return ( 
+    <div className='page'>
+      <header className='header'>
+        <img className='logo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIiqFJIIdkejClAqvMhQBtb9Jzia6wOKlkWw&usqp=CAU'/>
+        </header>
+
+      <sidebar className='sidebar'>
+        <div className='sidebar_nav'><a>Profile</a></div>
+        <div className='sidebar_nav'><a>Messages</a></div>
+        <div className='sidebar_nav'><a>News</a></div>
+        <div className='sidebar_nav'><a>Music</a></div>
+        <div className='sidebar_nav'><a>Settings</a></div>
+      </sidebar>
+      
+      <main className='main'>
+         <div>
+          <img className='main_photo' src='https://images.wallpaperscraft.ru/image/single/lodki_buhta_more_1081892_1920x1080.jpg'/>
+          </div>
+           <div>
+            ava + description
+           </div>
+          <div>
+          My posts
+          </div>
+          <div>
+            New post
+          </div>
+        <div>
     <div>
-      <Header />
-     <Technologies />
-     <Footer />
+      post 1
+    </div>
+    <div>
+      post 2
+    </div>
+        </div>
+
+      </main>
     </div>
   );
 }
