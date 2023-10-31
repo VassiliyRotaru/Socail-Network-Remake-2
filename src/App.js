06020e7feed1a1801a7ebe589b5e06ee8bb761b1
VassiliyRotar <vassiliyrotar@gmail.com>
2023-10-31 11:44:19 +0600

    components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,17 @@
 import React from 'react';  /* Если что удалить */
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Profile from './components/Profile';
 
 
 const App = () => { 
   return ( 
     <div className='page'>
-      <header className='header'>
-        <img className='logo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIiqFJIIdkejClAqvMhQBtb9Jzia6wOKlkWw&usqp=CAU'/>
-        </header>
-
-      <sidebar className='sidebar'>
-        <div className='sidebar_nav'><a>Profile</a></div>
-        <div className='sidebar_nav'><a>Messages</a></div>
-        <div className='sidebar_nav'><a>News</a></div>
-        <div className='sidebar_nav'><a>Music</a></div>
-        <div className='sidebar_nav'><a>Settings</a></div>
-      </sidebar>
-      
-      <main className='main'>
-         <div>
-          <img className='main_photo' src='https://images.wallpaperscraft.ru/image/single/lodki_buhta_more_1081892_1920x1080.jpg'/>
-          </div>
-           <div>
-            ava + description
-           </div>
-          <div>
-          My posts
-          </div>
-          <div>
-            New post
-          </div>
-        <div>
-    <div>
-      post 1
-    </div>
-    <div>
-      post 2
-    </div>
-        </div>
-
-      </main>
+      <Header />
+      <Sidebar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';  /* Если что удалить */
+
+
+const Sidebar = () => {
+return  <sidebar className='sidebar'>
+  <div className='sidebar_nav'><a>Profile</a></div>
+  <div className='sidebar_nav'><a>Messages</a></div>
+  <div className='sidebar_nav'><a>News</a></div>
+  <div className='sidebar_nav'><a>Music</a></div>
+  <div className='sidebar_nav'><a>Settings</a></div>
+</sidebar>
+
+}
+
+export default Sidebar; 
